baa017e19d765a7626ad497d080a887ce73fe214
Richard Duff <rduff@tvpage.com>
2018-04-25 16:49:05 -0700

    Removed compiled files - Removed the compiled artifacts from the build as these are actually release artifacts

--- TVP/build/TVP.build/Debug-iphonesimulator/TVP.build/Objects-normal/x86_64/TVP_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/devubhamanek/Desktop/Project\ Running/TVPage\ Player/TVPage_28_08_17/Source\ Code/TVPageSDK/TVP/build/TVP.build/Debug-iphonesimulator/TVP.build/DerivedSources/TVP_vers.c
--- build/tvp.build/release-iphoneos/TVP.build/Objects-normal/armv7/TvPageExtractor.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/devubhamanek/Desktop/TVPDemo/TVP/TVP/TvPageExtractor.m \
-  /Users/devubhamanek/Desktop/TVPDemo/TVP/build/TVP.build/Release-iphoneos/TVP.build/module.modulemap \
-  /Users/devubhamanek/Desktop/TVPDemo/TVP/TVP/TvPageExtractor.h
